4cdff0435041ee6c589e9470131c51e4a2acb3da
FRickReich <frederikreich@googlemail.com>
2020-10-24 22:49:08 +0200

    Create SlugifyString.ts

---
+++ src/server/Utils/SlugifyString.ts
@@ -0,0 +1,17 @@
+export const SlugifyString = (inputString : string) =>
+{
+    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
+    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------';
+    const p = new RegExp(a.split('').join('|'), 'g');
+
+    return inputString
+        .toString()
+        .toLowerCase()
+        .replace(/\s+/g, '-')
+        .replace(p, c => b.charAt(a.indexOf(c)))
+        .replace(/&/g, '-and-')
+        .replace(/[^\w\-]+/g, '')
+        .replace(/\-\-+/g, '-')
+        .replace(/^-+/, '')
+        .replace(/-+$/, '');
+};
